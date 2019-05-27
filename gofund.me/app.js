2680ef42975fc64ae76c8c1abc9cd94ed83e9a37
Jorge Oliveira <jorgedroliveira10@gmail.com>
2019-05-27 16:32:00 +0100

    update

--- gofund.me/app.js
+++ gofund.me/app.js
@@ -3,10 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var bodyParser = require('body-parser');
-var session = require('express-session');
-var passport = require('passport');
 var mongoose = require('mongoose');
+
 mongoose.Promise = global.Promise;
 
 mongoose.connect('mongodb://localhost/users', {useNewUrlParser: true})
@@ -28,12 +26,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({
-  secret: 'secret',
-  saveUninitialized: false,
-  resave: false
-}));
-
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
